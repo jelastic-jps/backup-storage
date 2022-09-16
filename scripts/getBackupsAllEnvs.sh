a2ad35bf573dfd3a6b7a12b7ea9daf9f0cd8a698
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-09-16 12:38:47 +0300

    Update getBackupsAllEnvs.sh

--- scripts/getBackupsAllEnvs.sh
+++ scripts/getBackupsAllEnvs.sh
@@ -10,7 +10,8 @@ if [ -n "$ENV_LIST" ]; then
 
     for i in $(ls /data)
     do
-        DIRECTORY_LIST=$(ls -Qm /data/${i})
+        DIRECTORY_LIST=$(RESTIC_PASSWORD="$i" restic -r /data/$i snapshots|awk '{print $5}'|grep -o [0-9_-]*|awk '{print "\""$1"\""}'|tr '\n' ',')
+DIRECTORY_LIST=${DIRECTORY_LIST::-1}
         OUTPUT_JSON="${OUTPUT_JSON}\"${i}\":[${DIRECTORY_LIST}],"
     done
 
